602c778a24655ff3e75d45a490ec3e18012fb588
wbuz24 <wbuziak@vols.utk.edu>
2023-05-23 20:42:52 -0400

    cleanup

--- src/gui-test.py
+++ src/gui-test.py
@@ -1,48 +1,21 @@
-from PyQt5.QtWidgets import * 
-from PyQt5 import QtCore, QtGui
-from PyQt5.QtGui import * 
-from PyQt5.QtCore import * 
-import sys
-  
-class Window(QMainWindow):
-  
-    def __init__(self):
-        super().__init__()
-  
-        # setting title
-        self.setWindowTitle("Python ")
-  
-        # setting geometry
-        self.setGeometry(100, 100, 500, 400)
-  
-        # calling method
-        self.UiComponents()
-  
-        # showing all the widgets
-        self.show()
-  
-  
-    # method for components
-    def UiComponents(self):
-  
-  
-        # creating a command link button
-        cl_button = QCommandLinkButton("GeeksforGeeks", self)
-  
-        # setting geometry
-        cl_button.setGeometry(200, 100, 200, 50)
-  
-        # setting animate click property
-        cl_button.animateClick(200)
-  
-          
-  
-  
-# create pyqt5 app
-App = QApplication(sys.argv)
-  
-# create the instance of our Window
-window = Window()
-  
-# start the app
-sys.exit(App.exec())
+from PyQt5.QtWidgets import *
+def main(): 
+    app =QApplication([]) # create a window
+    window = QWidget()
+    
+    runButton = QPushButton(window) # run button
+    runButton.setText("Run")
+    runButton.move(50, 50)
+
+    exitButton = QPushButton(window) # exit button
+    exitButton.setText("Exit")
+    exitButton.move(50, 100)
+
+    window.setGeometry(100, 100, 320, 200)
+    window.setWindowTitle("Golf balance window")
+    window.show() # show window
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
